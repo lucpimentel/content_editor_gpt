22b641496c1a6f181451505796ac48bbf4ba326a
lucpimentel <123824068+lucpimentel@users.noreply.github.com>
2023-05-07 16:09:54 -0300

    Update content_editor.py

--- content_editor.py
+++ content_editor.py
@@ -21,6 +21,7 @@ I will give you one of my draft paragraphs and you will correct it for me.
 Please abide to the following rules:
 - Keep your language simple and clear
 - Write in direct form
+- Prefer to use nouns and verbs instead of adjectives and adverbs
 
 Draft paragraph: {draft_paragraph}
 Corrected paragraph:'''
